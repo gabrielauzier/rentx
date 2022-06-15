7b6aaf6501692d3cfe3ddc545ae7e749b4782a02
gabvask <gabrielvascoauzier@gmail.com>
2022-06-15 14:53:56 -0400

    feat: add Home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,43 @@
 import React from "react";
-import { Container, Title } from "./styles";
-import DoneIcon from "../../assets/done.svg";
+import { StatusBar } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 
+import Logo from "../../assets/logo.svg";
+import { Car } from "../../components/Car";
+
+import { Container, Header, HeaderContent, TotalCars, CarList } from "./styles";
+
 export function Home() {
+  const carData = {
+    brand: "AUDI",
+    name: "RS 5 Coupé",
+    rent: {
+      period: "AO DIA",
+      price: 120,
+    },
+    thumbnail:
+      "https://platform.cstatic-images.com/xlarge/in/v2/stock_photos/ff5a98a2-fd1e-4585-84a9-d91a5947d7d0/61f4cdfb-46ba-4ae9-8c08-3414e91094af.png",
+  };
+
   return (
     <Container>
-      <DoneIcon width={RFValue(36)} />
-      <Title>
-        Welcome, bro! This is a start template app bootstrapped with Expo Bare
-        Workflow and TypeScript
-      </Title>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
+      </Header>
+
+      <CarList
+        data={[1, 2, 3, 4, 5, 6, 7, 8]}
+        keyExtractor={(item) => String(item)}
+        renderItem={({ item }) => <Car data={carData} />}
+      />
     </Container>
   );
 }
