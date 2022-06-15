f1e09b03ce8bccec66912494f3c249f60da56c6d
gabvask <gabrielvascoauzier@gmail.com>
2022-06-15 19:44:53 -0400

    feat: add ScheduliingCompleted screen

---
+++ src/screens/SchedulingCompleted/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useWindowDimensions, StatusBar } from "react-native";
+import LogoSvg from "../../assets/logo_background_gray.svg";
+import DoneSvg from "../../assets/done.svg";
+
+import { Container, Content, Title, Message, Footer } from "./styles";
+import { ConfirmButton } from "../../components/ConfirmButton";
+
+export function SchedulingCompleted() {
+  const { width } = useWindowDimensions();
+
+  return (
+    <Container>
+      <StatusBar barStyle="light-content" translucent />
+      <LogoSvg width={width} />
+
+      <Content>
+        <DoneSvg width={80} />
+        <Title>Carro alugado!</Title>
+
+        <Message>
+          Agora você só precisa ir {"\n"}
+          até a concessionária da RENTX {"\n"}
+          pegar o seu automóvel.
+        </Message>
+      </Content>
+      <Footer>
+        <ConfirmButton title="OK" />
+      </Footer>
+    </Container>
+  );
+}
